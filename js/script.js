15bbb2ba415f4dc1ff7b0ad09c21c38a1aee9c60
PatGal <paterson.nya@gmail.com>
2023-03-22 12:23:22 +0300

    Firts changes

---
+++ js/script.js
@@ -0,0 +1,43 @@
+"use strict"
+
+// const result = confirm('Vous etes la???');
+// console.log(result);
+
+// const answer = prompt('Vous avez 18ans???', '18')
+// console.log(typeof(answer));
+// const answers = [];
+// answers[0] = prompt('Quel est votre nom?', '')
+// answers[0] = prompt('Quel est votre prenom?', '')
+// answers[0] = prompt('Quel est votre age?', '')
+// console.log(typeof(answers));
+// console.log('PatGal');
+// console.log(`2 + 2 = ${2+5}`);
+
+// const ourPlanetName = '';
+// const userTime = '';
+// const storyCount = '';
+// const payCashOrNot = '';
+// console.log('Hello world');
+//const userName = 'PatGal';
+//console.log(`Welcome ${userName}`);
+// const test = true;
+// console.log(typeof test, test);
+// console.log(typeof String(test), test);
+// const teste = 45;
+// console.log(typeof teste, teste);
+// console.log(typeof String(teste), teste);
+// const test = null;
+// console.log(typeof test, test);
+// console.log(typeof Number(test), Number(test));
+// const test = '123e3';
+// console.log(typeof test, test);
+// console.log(typeof Number(test), Number(test));
+// const test = '';
+// console.log(typeof test, test);
+// console.log(typeof Boolean(test), Boolean(test));
+const var1 = 2;
+const  var2 = '3';
+console.log(var1 < var2);
+const a = 'ананас';
+const b ='яблоко';
+console.log(a.charCodeAt(), b.charCodeAt());
